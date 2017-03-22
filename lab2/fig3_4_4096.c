bdf82916244a08f19ef9b5fddd8c4d93f80c3d40
p5351280 <p5351280@gmail.com>
2017-03-22 15:13:00 +0800

    Lab2

---
+++ lab2/fig3_4_4096.c
@@ -0,0 +1,22 @@
+#include "apue.h"
+#include<fcntl.h>
+#define	BUFFSIZE	4096
+
+int main(void)
+{
+		int fd;
+		fd=open("/dev/null", O_RDWR);
+		
+		int		n;
+		char	buf[BUFFSIZE];
+
+		while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+				if (write(fd, buf, n) != n)
+						err_sys("write error");
+
+		if (n < 0)
+				err_sys("read error");
+
+		exit(0);
+}
+
