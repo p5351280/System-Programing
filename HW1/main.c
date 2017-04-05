dfee0c6f72a05e3ae45af1b0a95bb34fe9c16256
p5351280 <p5351280@gmail.com>
2017-04-06 02:32:43 +0800

    first complete

--- HW1/main.c
+++ HW1/main.c
@@ -24,8 +24,6 @@ char* Lower(char []);
 char* Reverse(char []);
 int check(int [], int);
 
-int stk[SIZE], top;
-
 int main(int argc, char* argv[]){
 	if(freopen(argv[1], "r", stdin) == NULL){
 		printf("ERROR: File Not Found!\n");
@@ -42,55 +40,135 @@ int main(int argc, char* argv[]){
 	}
 	int line = 0;
 	char oneLine[SIZE];
-	printf("%d, %d", recognize("<Proper>1234"), recognize("</Proper>123"));
-	/*
-	   while(fgets(oneLine, SIZE, stdin)){
-	   memset(stk, 0, sizeof(stk));
-	   top = -1;
-	   len = strlen(oneLine);
-	   if(oneLine[len-1] == '\n')
-	   oneLine[len-1] = 0;
-	   puts(operation(oneLine));
-	   }
-	 */
+	while(fgets(oneLine, SIZE, stdin)){
+		line++;
+		len = strlen(oneLine);
+		if(oneLine[len-1] == '\n')
+			oneLine[len-1] = 0;
+		if(strstr(operation(oneLine), "ERROR"))
+			printf("%s in line %d in file %s\n", operation(oneLine), line, argv[1]);
+		else
+			puts(operation(oneLine));
+	}
+	//puts(operation("Wow<Insert char = '!' num = '3'> That's a good idea!"));
+	//puts(operation("This is line 1. <Newline>This is line 2."));
+	//puts(operation("Wow!!! That's a <Reverse>DOOG</Reverse> idea!"));
+
 	return 0;
 }
 
 char* operation(char input[]){
-	char *temp;
-	while((temp = strchr(input, '<'))){
+	char *start, *end,  *ptr[SIZE], stkInput[SIZE][SIZE]={{0}};
+	int top=0, final=0, stk[SIZE]={0};
+	static char result[SIZE];
+	memset(result, 0, sizeof(result));
+	int i, len = strlen(input);
+	start = strchr(input, '<');
+	if(start){
+		strncpy(stkInput[top], input, start-input);
 
+		while(start){	
+			int kind = recognize(start);
+			if(kind < 0){
+				return "ERROR: wrong tags";
+			}
+			else if(kind == INSERT){
+				end = strchr(start, '>');
+				char temp[SIZE]={0};
+				strncpy(temp, start, end-start+1);
+				strcpy(stkInput[top]+strlen(stkInput[top]), Insert(temp));
+				start = strchr(end+1, '<');
+			}
+			else if(kind == NEWLINE){
+				strcpy(stkInput[top]+strlen(stkInput[top]), "\n");
+				end = strchr(start, '>');
+				start = strchr(start+1, '<');
+			}
+			else if(kind < ENDPROPER){
+				stk[++top] = kind;
+				end = strchr(start, '>');
+				if(!strncmp(end+1, "<", 1)){
+					start = end+1;
+					continue;
+				}
+				else{
+					start = strchr(end, '<');
+					strncpy(stkInput[top], end+1, start-end-1);
+				}
+				final = top;
+			}
+			else{
+				if(stk[top] != kind-4)
+					return "ERROR: Pairs of tags are interleaved!";
+				else{
+					int choose = stk[top];
+					switch(choose){
+						case PROPER:
+							for(i=top; i<=final; i++)
+								strcpy(stkInput[i], Proper(stkInput[i]));
+							break;
+						case UPPER:
+							for(i=top; i<=final; i++)
+								strcpy(stkInput[i], Upper(stkInput[i]));
+							break;
+						case LOWER:
+							for(i=top; i<=final; i++)
+								strcpy(stkInput[i], Lower(stkInput[i]));
+							break;
+						case REVERSE:
+							for(i=top; i<=final; i++)
+								strcpy(stkInput[i], Reverse(stkInput[i]));
+							break;
+					}
+					top--;
+				}
+				end = strchr(start, '>');
+				start = strchr(start+1, '<');
+			}
+		}
+		if(top!=0)
+			return "ERROR: The tag is not a pair.";
+
+		for(i=0; i<=final; i++){
+			strcpy(result+strlen(result), stkInput[i]);
+			//	puts(stkInput);
+		}
+		strcpy(result+strlen(result), end+1);
 	}
+	else
+		strcpy(result, input);
+	return result;
 }
 
 int recognize(char input[]){
 	input += 1;
 	if(!strncmp(input, "Insert", 6))
 		return INSERT;
-	else if(!strncmp(input, "Newline", 7))
+	else if(!strncmp(input, "Newline>", 8))
 		return NEWLINE;
-	else if(!strncmp(input, "Proper", 6))
+	else if(!strncmp(input, "Proper>", 7))
 		return PROPER;
-	else if(!strncmp(input, "Upper", 5))
+	else if(!strncmp(input, "Upper>", 6))
 		return UPPER;
-	else if(!strncmp(input, "Lower", 5))
+	else if(!strncmp(input, "Lower>", 6))
 		return LOWER;
-	else if(!strncmp(input, "Reverse", 7))
+	else if(!strncmp(input, "Reverse>", 8))
 		return REVERSE;
 	else{
 		input += 1;
-		if(!strncmp(input, "Proper", 6))
+		if(!strncmp(input, "Proper>", 7))
 			return ENDPROPER;
-		else if(!strncmp(input, "Upper", 5))
+		else if(!strncmp(input, "Upper>", 6))
 			return ENDUPPER;
-		else if(!strncmp(input, "Lower", 5))
+		else if(!strncmp(input, "Lower>", 6))
 			return ENDLOWER;
-		else if(!strncmp(input, "Reverse", 7))
+		else if(!strncmp(input, "Reverse>", 8))
 			return ENDREVERSE;
 		else
 			return -1;
 	}
 }
+
 char* Insert(char input[]){
 	static char result[SIZE];
 	memset(result, 0, sizeof(result));
@@ -106,6 +184,8 @@ char* Insert(char input[]){
 	temp_2 = strchr(temp_1+1, '\'');
 	strncpy(nu, temp_1+1, temp_2-temp_1-1);
 	int num = atoi(nu);
+
+	/* find for the end */
 
 	for(int i=0; i<num; i++)
 		strcpy(result+strlen(result), ch);
