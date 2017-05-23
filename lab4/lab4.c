3267ba56c30eb3dd8efd6f937c8e97878dd745c2
p5351280 <p5351280@gmail.com>
2017-05-24 02:27:04 +0800

    add lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -16,117 +16,117 @@ int main()
 	int year=5, week=52, day=7;
 	char filename[1000];
 	int i, j, k, temp, total=0;
-    sum = fopen("sum.txt", "w");
-    fprintf(sum, "%d\n", 0);
-    fclose(sum);
-    TELL_WAIT();
-    child[0] = fork();
-    if(child[0] == 0){
-        for(k=1; k<=week; k++){
-            total=0;
-            sprintf(filename, "1-%02d.txt", k);
-            input = fopen(filename, "r");
-            for(i=0; i<day; i++){
-                total=0;
-                for(j=0; j<96; j++){
-                    fscanf(input, "%d", &temp);
-                    total += temp;
-                }
-                accumulation(total);
-            }
-        }
-        TELL_PARENT(getppid());
-        exit(0);
-    }
-    
-    WAIT_CHILD();
-    child[1] = fork();
+	sum = fopen("sum.txt", "w");
+	fprintf(sum, "%d\n", 0);
+	fclose(sum);
+	TELL_WAIT();
+	child[0] = fork();
+	if(child[0] == 0){
+		for(k=1; k<=week; k++){
+			total=0;
+			sprintf(filename, "1-%02d.txt", k);
+			input = fopen(filename, "r");
+			for(i=0; i<day; i++){
+				total=0;
+				for(j=0; j<96; j++){
+					fscanf(input, "%d", &temp);
+					total += temp;
+				}
+				accumulation(total);
+			}
+		}
+		TELL_PARENT(getppid());
+		exit(0);
+	}
 
-    if(child[1]==0){
-        for(k=1; k<=week; k++){
-            total=0;
-            sprintf(filename, "2-%02d.txt", k);
-            input = fopen(filename, "r");
-            for(i=0; i<day; i++){
-                total=0;
-                for(j=0; j<96; j++){
-                    fscanf(input, "%d", &temp);
-                    total += temp;
-                }
-                accumulation(total);
-            }
-        }
-        TELL_PARENT(getppid());
-        exit(0);
-    }
-    
-    WAIT_CHILD();
-    child[2] = fork();
-	
-    if(child[2]==0){
-        for(k=1; k<=week; k++){
-            total=0;
-            sprintf(filename, "3-%02d.txt", k);
-            input = fopen(filename, "r");
-            for(i=0; i<day; i++){
-                total=0;
-                for(j=0; j<96; j++){
-                    fscanf(input, "%d", &temp);
-                    total += temp;
-                }
-                accumulation(total);
-            }
-        }
-        TELL_PARENT(getppid());
-        exit(0);
-    }
-    
-    WAIT_CHILD();
-    child[3] = fork();
-	
-    if(child[3]==0){
-        for(k=1; k<=week; k++){
-            total=0;
-            sprintf(filename, "4-%02d.txt", k);
-            input = fopen(filename, "r");
-            for(i=0; i<day; i++){
-                total=0;
-                for(j=0; j<96; j++){
-                    fscanf(input, "%d", &temp);
-                    total += temp;
-                }
-                accumulation(total);
-            }
-        }
-        TELL_PARENT(getppid());
-        exit(0);
-    }
+	WAIT_CHILD();
+	child[1] = fork();
 
-    WAIT_CHILD();
-    child[4] = fork();
-    
-    if(child[4]==0){
-        for(k=1; k<=week; k++){
-            total=0;
-            sprintf(filename, "5-%02d.txt", k);
-            input = fopen(filename, "r");
-            for(i=0; i<day; i++){
-                total=0;
-                for(j=0; j<96; j++){
-                    fscanf(input, "%d", &temp);
-                    total += temp;
-                }
-                accumulation(total);
-            }
-        }
-        TELL_PARENT(getppid());
-        exit(0);
-    }
+	if(child[1]==0){
+		for(k=1; k<=week; k++){
+			total=0;
+			sprintf(filename, "2-%02d.txt", k);
+			input = fopen(filename, "r");
+			for(i=0; i<day; i++){
+				total=0;
+				for(j=0; j<96; j++){
+					fscanf(input, "%d", &temp);
+					total += temp;
+				}
+				accumulation(total);
+			}
+		}
+		TELL_PARENT(getppid());
+		exit(0);
+	}
 
-    WAIT_CHILD();
+	WAIT_CHILD();
+	child[2] = fork();
+
+	if(child[2]==0){
+		for(k=1; k<=week; k++){
+			total=0;
+			sprintf(filename, "3-%02d.txt", k);
+			input = fopen(filename, "r");
+			for(i=0; i<day; i++){
+				total=0;
+				for(j=0; j<96; j++){
+					fscanf(input, "%d", &temp);
+					total += temp;
+				}
+				accumulation(total);
+			}
+		}
+		TELL_PARENT(getppid());
+		exit(0);
+	}
+
+	WAIT_CHILD();
+	child[3] = fork();
+
+	if(child[3]==0){
+		for(k=1; k<=week; k++){
+			total=0;
+			sprintf(filename, "4-%02d.txt", k);
+			input = fopen(filename, "r");
+			for(i=0; i<day; i++){
+				total=0;
+				for(j=0; j<96; j++){
+					fscanf(input, "%d", &temp);
+					total += temp;
+				}
+				accumulation(total);
+			}
+		}
+		TELL_PARENT(getppid());
+		exit(0);
+	}
+
+	WAIT_CHILD();
+	child[4] = fork();
+
+	if(child[4]==0){
+		for(k=1; k<=week; k++){
+			total=0;
+			sprintf(filename, "5-%02d.txt", k);
+			input = fopen(filename, "r");
+			for(i=0; i<day; i++){
+				total=0;
+				for(j=0; j<96; j++){
+					fscanf(input, "%d", &temp);
+					total += temp;
+				}
+				accumulation(total);
+			}
+		}
+		TELL_PARENT(getppid());
+		exit(0);
+	}
+
+	WAIT_CHILD();
 	sum = fopen("sum.txt", "r");
 	fscanf(sum, "%d", &total_sum);
-    //printf("%s, %d\n", filename, total_sum);
+	//printf("%s, %d\n", filename, total_sum);
 	printf("Day_Average = %d\n",total_sum/(year*week*day));
 
 	fclose(sum);
